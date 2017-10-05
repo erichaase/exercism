4744254a3ffbdc44ea6b9f4a7319437fa2150e36
Eric Haase <erichaase@gmail.com>
2017-10-04 20:45:49 -0500

    Added elixir/nucleotide-count solution

--- elixir/nucleotide-count/nucleotide_count.exs
+++ elixir/nucleotide-count/nucleotide_count.exs
@@ -14,9 +14,8 @@ defmodule NucleotideCount do
   """
   @spec count([char], char) :: non_neg_integer
   def count(strand, nucleotide) do
-
+    Enum.count(strand, &(&1 == nucleotide))
   end
-
 
   @doc """
   Returns a summary of counts by nucleotide.
@@ -28,6 +27,13 @@ defmodule NucleotideCount do
   """
   @spec histogram([char]) :: map
   def histogram(strand) do
-
+    map = %{?A => 0, ?T => 0, ?C => 0, ?G => 0}
+    Enum.reduce strand, map, fn(x, m) ->
+      if c = Map.get(m, x) do
+        Map.put(m, x, c + 1)
+      else
+        Map.put(m, x, 1)
+      end
+    end
   end
 end
