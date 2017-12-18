6692e6a643fb9761e5280b7ca5237c4317ed48e8
Eric Haase <erichaase@gmail.com>
2017-12-17 20:07:12 -0600

    Added python/isogram solution

--- python/isogram/isogram.py
+++ python/isogram/isogram.py
@@ -1,2 +1,10 @@
 def is_isogram(string):
-    pass
+    counts = {}
+    for char in string.lower():
+        if char in ' -':
+            continue
+        elif char in counts:
+            return False
+        else:
+            counts[char] = 1
+    return True
