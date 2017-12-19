4076a0e0bc1dca3248f86b0b07ffc88a71f6154f
Eric Haase <erichaase@gmail.com>
2017-12-19 00:35:22 -0600

    Added python/pangram solution

--- python/pangram/pangram.py
+++ python/pangram/pangram.py
@@ -1,2 +1,4 @@
 def is_pangram(sentence):
-    pass
+    alphabet = set(range(ord('a'), ord('z') + 1))
+    chars = set([ord(c) for c in list(sentence.lower()) if ord(c) in alphabet])
+    return True if alphabet == chars else False
