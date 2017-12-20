b410ff8ad9ab2dda6b3ee4a0f3ad5dc12d1278c4
Eric Haase <erichaase@gmail.com>
2017-12-20 08:27:02 -0600

    Added go/leap solution

--- go/leap/leap.go
+++ go/leap/leap.go
@@ -2,5 +2,14 @@ package leap
 
 const testVersion = 3
 
-func IsLeapYear(int) bool {
+func IsLeapYear(y int) bool {
+	if y % 400 == 0 {
+		return true
+	} else if y % 100 == 0 {
+		return false
+	} else if y % 4 == 0 {
+		return true
+	} else {
+		return false
+	}
 }
